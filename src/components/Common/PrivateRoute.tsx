d1b3af2afbc6ddf446c60d2d189ca3bf9830a928
lenh0205 <lenhdeptrai98@gmail.com>
2022-07-20 13:31:11 +0700

    14 - Setup routings

---
+++ src/components/Common/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navigate, Route, RouteProps } from 'react-router-dom';
+
+export const PrivateRoute = (children: any) => {
+  // Check if user is logged in
+  // If yes, show route
+  // Otherwise, redirect to login page
+  const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+  
+  if (!isLoggedIn) return <Navigate to="/login"/>
+  return children;
+};
---
+++ src/components/Layout/Admin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export interface AdminLayoutProps {}
+
+export const AdminLayout = (props: AdminLayoutProps) => {
+  return (
+    <div>Admin</div>
+  )
+}
